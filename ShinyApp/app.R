bcc976d18b86ed562daa489222b39602ddf84e1d
Pride-Akana-Techa <techap@berea.edu>
2026-06-09 15:52:51 -0400

    Added more tabs on the dashboard and modified the overview tab.

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -10,7 +10,9 @@ library(plotly)
 
 ui <- navbarPage(
   title = div(
-    "DSPG-26",
+    "California's Daylighting Law (AB 413)",
+    div("Investigating the Impacts of Daylighting on Pedestrian and Bicyclist  Safety", 
+        style = "font-size: 13px;"),
     tags$img(
       src = "Virginia-Tech-Logo.png",
       height = "40px",
@@ -22,35 +24,63 @@ ui <- navbarPage(
     "Overview",
     
     fluidRow(
-      "On October 10th, 2023, ",
-      tags$a(
-        href = "https://leginfo.legislature.ca.gov/faces/billTextClient.xhtml?bill_id=202320240AB413",
-        "California Assembly Bill 413 (AB-413)",
-        target = "_blank"
-      ),
-      " was signed into law. The state-wide law prohibits parking within 20 feet on the approach side of an intersection. Commonly referred to as ",
-      tags$i("daylighting"),
-      ". This project aims to investigate the effectiveness of California's daylighting law on improving pedestrian and bicyclist safety at intersections."
-    ),
-    
-    br(),
-    
-    fluidRow(
       column(
         6,
-        p("A. Total Yearly Deaths of Pedestrians at Intersections in California"),
-        img(src = "yearly_ped_int_death.png", width = "100%"),
-        p("This figure shows a drastic decrease in the number of deaths of pedestrians at intersections from January 2024, when the law was enforced. It is also observed that the number of deaths continued to decline after January 2025, when enforcement moved from warning based to citation based.")
+        
+        wellPanel(
+          
+          h4("About the Law"),
+          
+          p(
+            "On October 10, 2023, ",
+            tags$a(
+              href = "https://leginfo.legislature.ca.gov/faces/billTextClient.xhtml?bill_id=202320240AB413",
+              "California Assembly Bill 413 (AB 413)",
+              target = "_blank"
+            ),
+            " was signed into law."
+          ),
+          
+          p(
+            "The statewide law prohibits parking within 20 feet of the approach side of marked and unmarked crosswalks at intersections, a practice commonly referred to as ",
+            tags$i("daylighting"),
+            "."
+          ),
+          
+          h5("Why Was the Law Enacted?"),
+          
+          tags$ul(
+            tags$li("Improve sight distance at intersections"),
+            tags$li("Reduce pedestrian and bicyclist crashes"),
+            tags$li("Increase driver awareness of crossing road users")
+          )
+          
+        )
       ),
       
       column(
         6,
-        p("B. Total Monthly Deaths of Pedestrians at Intersections in California"),
-        img(src = "monthly_ped_int_death.png", width = "100%"),
-        p("In the monthly trend, the total number of deaths fluctuates throughout each year due to seasonal changes, but overall, there is a sharp decrease from the start of 2014 to the end of 2025.")
+        
+        div(
+          class = "well",
+          
+          h4("About this Study"),
+          
+          p(
+            "This project investigates the effectiveness of California's daylighting law in improving pedestrian and bicyclist safety at intersections."
+          ),
+          
+          h5("Research Questions"),
+          
+          tags$ul(
+            tags$li("Have pedestrian and bicycle crashes changed since AB 413 was implemented?"),
+            tags$li("Do impacts vary across counties and regions?"),
+            tags$li("Are impacts distributed equitably across demographic groups?"),
+            tags$li("Does enforcement intensity influence outcomes?")
+          )
+        )
       )
     ),
-    
     titlePanel("Interactive Graph"),
     
     sidebarLayout(
@@ -90,16 +120,31 @@ ui <- navbarPage(
       )
     )
   ),
+
   
   tabPanel(
-    "Literature Review",
-    fluidPage(
-      # you can add content here later
-    )
+    "Before/After Analysis",
+    fluidRow(
+      column(
+        6,
+        p("A. Total Yearly Deaths of Pedestrians at Intersections in California"),
+        img(src = "yearly_ped_int_death.png", width = "100%"),
+        p("This figure shows a drastic decrease in the number of deaths of pedestrians at intersections from January 2024, when the law was enforced. It is also observed that the number of deaths continued to decline after January 2025, when enforcement moved from warning based to citation based.")
+      ),
+      
+      column(
+        6,
+        p("B. Total Monthly Deaths of Pedestrians at Intersections in California"),
+        img(src = "monthly_ped_int_death.png", width = "100%"),
+        p("In the monthly trend, the total number of deaths fluctuates throughout each year due to seasonal changes, but overall, there is a sharp decrease from the start of 2014 to the end of 2025.")
+      )
+    ),
   ),
   
   tabPanel("Maps"),
-  tabPanel("Analysis")
+  tabPanel("Results"),
+  tabPanel("Data Sources"),
+  tabPanel("About Us")
 )
 
 server <- function(input, output) {}
