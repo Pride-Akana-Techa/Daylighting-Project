7ec13a71e63e322a4d3b2ed0a2371bc54b59c3b0
Pride-Akana-Techa <techap@berea.edu>
2026-06-25 16:59:03 -0400

    Refined RDiT model and combined weather data

--- initial-analysis/scripts/TIMS-Analysis/RDiT-model.R
+++ initial-analysis/scripts/TIMS-Analysis/RDiT-model.R
@@ -9,48 +9,60 @@
 # Load packages
 library(tidyverse)
 library(rdrobust) # for RDiT analysis and plots
-library(lmtest) # to run a Durbin-Watson test
-library(sandwich) # to compute HAC standard errors
+library(rddensity) 
 
 # Read datasets
 tims_crashes <-  readRDS("initial-analysis/data/TIMS_Filtered.rds")
 ccrs_crashes <-  read_csv("initial-analysis/dat/updated-crashes.csv")
 
 
-# RDiT Model: Jan 1, 2024 -------------------------------------------------
 
+# Initial Monthly Crash Plot ----------------------------------------------
 
-# TIMS Plot of crashes
-
-tims_months = c(
-  "1" = "January", "2" = "February", "3" = "March", "4" = "April",
-  "5" = "May", "6" = "June", "7" = "July", "8" = "August",
-  "9" = "September", "10" = "October", "11" = "November", "12" = "December" 
-)
-
-# Initial plot of crashes
-tims_crashes |> 
+# Create the plot data
+plot_data <- tims_crashes |> 
   filter(ACCIDENT_YEAR >= 2022) |> 
   filter(PED_ACTION == "B" & INTERSECTION == "Y") |> 
   mutate(MONTH = floor_date(ymd(COLLISION_DATE), "month"),
          Time = interval(as.Date("2024-01-01"), MONTH) %/% months(1),
          Post = ifelse(Time >= 0, 1, 0)
   ) |> 
-  group_by(ACCIDENT_YEAR, Time, PED_ACTION, INTERSECTION, Post) |> 
-  summarise(TOTAL_CRASHES = n(),
-            .groups = "drop") |> 
-  ggplot(aes(x = Time, y = TOTAL_CRASHES)) +
+  count(Time, Post, name = "Total_crashes")
+
+# Plot the monthly trends
+  ggplot(plot_data,
+         aes(x = Time, y = Total_crashes)) +
   geom_point() +
-  geom_smooth() +
-  geom_vline(xintercept = -0.5, 
-             linetype = "dashed", 
+  
+  geom_smooth(data = subset(plot_data, Post == 0),
+              method = "lm",
+              formula = y ~ x + I(x^2),
+              se = FALSE) +
+  
+  geom_smooth(data = subset(plot_data, Post == 1),
+              method = "lm",
+              formula = y ~ x + I(x^2),
+              se = FALSE) +
+  
+  geom_vline(xintercept = -0.5,
+             linetype = "dashed",
              linewidth = 1) +
-  theme_minimal(base_size = 13) +
-  scale_x_continuous(breaks = seq(-24, 23, by = 1)) +
+    
+    theme_minimal(base_size = 13) +
+    scale_x_continuous(breaks = seq(-24, 23, by = 1)) +
+    scale_color_viridis_d() +
+  
   labs(title = "Model of Crashes Involving Pedestrians at Intersections",
-       x = "Time",
-       y = "Number of Crashes",
-       caption = "Source: TIMS")
+       x = "Months Relative to Jan 01, 2024",
+       y = "Monthly Pedestrian Crashes") 
+  
+# Save plot
+ggsave(filename = "initial-analysis/figs/initial-lm.png",
+       width = 20,
+       height = 10)
+
+
+# Prepare Data for Model --------------------------------------------------
 
 # Cleaning and organizing the data with selected features
 tims_months = c(
@@ -78,172 +90,169 @@ tims_road_surface = c(
   "A" = "Dry", "B" = "Wet", "C" = "Snowy/Icy", "D" = "Other"
 )
 
-tims_updated <- tims_updated |> 
+tims_updated <- tims_crashes |> 
   mutate(
-    lighting  = tims_lighting[as.character(lighting)],
-    weather = tims_weather[as.character(weather)],
-    day = tims_days[as.character(day)],
-    month = tims_months[as.character(month)],
-    Source = "TIMS",
-    county_name = str_to_title(trimws(county_name))
+    LIGHTING  = tims_lighting[as.character(LIGHTING)],
+    WEATHER_1 = tims_weather[as.character(WEATHER_1)],
+    DAY_OF_WEEK = tims_days[as.character(DAY_OF_WEEK)],
   )
 
+
 # Create the data for the model
-rdit_data <- tims_crashes |> 
+rdit_data <- tims_updated |> 
   filter(ACCIDENT_YEAR >= 2022) |> 
-  filter(PED_ACTION == "B" & INTERSECTION == "Y") |> 
+  filter(PED_ACTION == "B" & INTERSECTION == "Y") |>
+  filter_out(is.na(WEATHER_1)) |> 
+  filter_out(is.na(LIGHTING)) |> 
   mutate(MONTH = floor_date(ymd(COLLISION_DATE), "month"),
+         Month_factor = factor((Time %% 12 + 12) %% 12),
          Time = interval(as.Date("2024-01-01"), MONTH) %/% months(1),
          Post = ifelse(Time >= 0, 1, 0)
         ) |> 
-  group_by(Time, Post) |> 
+  group_by(Time, Post, Month_factor) |> 
   summarise(Total_crashes = n(),
             .groups = "drop")
 
-# Running the model 
-model_ols = lm(Total_crashes ~ Time + Post,
-               data = rdit_data)
 
-summary(model_ols)
+# Run Model ---------------------------------------------------------------
 
+## 1. Linear Model
 
 # Run the model
-rdit_model <- lm(
-  Total_crashes ~ Time + Post + Time*Post,
-  data = rdit_data
-)
+c <- 0  # setting the cutoff
+rdit_model <- lm(Total_crashes ~ Post + I(Time - c) + Post * I(Time - c) +                    Month_factor,
+                 data = rdit_data)
 
 summary(rdit_model)
 
-# Plot the model
-ggplot(rdit_data, aes(x = Time, y = Total_crashes)) +
-  geom_point() +
+# Sensitivity Check
+
+tibble(Model = c("Narrow (±9mo)", "Full (±24mo)", "Wide (±18mo)"),
+       Estimate = c(
+         coef(lm(Total_crashes ~ Post + I(Time - c) + Post * I(Time - c) + Month_factor,
+                 data = filter(rdit_data, Time >= -9 & Time <= 9)))["Post:I(Time - c)"],
+         
+         coef(rdit_model)["Post:I(Time - c)"],
+         
+         coef(lm(Total_crashes ~ Post + I(Time - c) + Post * I(Time - c) + Month_factor,
+                 data = filter(rdit_data, Time >= -18 & Time <= 18)))["Post:I(Time - c)"]
+       ),
+       
+       P_value = c(
+         summary(lm(Total_crashes ~ Post + I(Time - c) + Post * I(Time - c) + Month_factor,
+                    data = filter(rdit_data, Time >= -9 & Time <= 9)))$coefficients["Post:I(Time - c)", 4],
+         
+         summary(rdit_model)$coefficients["Post:I(Time - c)", 4],
+         
+         summary(lm(Total_crashes ~ Post + I(Time - c) + Post * I(Time - c) + Month_factor,
+                    data = filter(rdit_data, Time >= -18 & Time <= 18)))$coefficients["Post:I(Time - c)", 4]
+       ))
+
+
+
+## 2. Using rdrobust
+
+rd_model <- rdrobust(y = rdit_data$Total_crashes,
+                     x = rdit_data$Time,
+                     covs = model.matrix(~ Month_factor, rdit_data)[, -1],
+                     c = 0,
+                     p = 2,
+                     h = 24,
+                     kernel = "uniform",
+                     bwselect = "mserd")
+
+summary(rd_model)
+
+
+# Plot Model --------------------------------------------------------------
+ggplot(rdit_data,
+       aes(Time, Total_crashes)) +
+  geom_point(size = 2) +
   
-  # Before policy trend
-  geom_smooth(data = filter(rdit_data, Time < 0),
-              method = "lm", 
-              formula = y ~ x + I(x^2),
-              se = TRUE) +
-  # After policy trend
-  geom_smooth(data = filter(rdit_data, Time >= 0),
+  geom_smooth(data = subset(rdit_data, Post == 0),
               method = "lm",
-              formula = y ~ x + I(x^2),
               se = TRUE) +
-  geom_vline(xintercept = 0,
-            linetype = "dashed") +
-  scale_x_continuous(breaks = seq(-12,12,1)) +
-  theme_minimal() +
-  scale_color_viridis_d() +
-  labs(
-    title = "RDiT Model of Pedestrian Crashes at Intersections",
-    x = "Months Relative to Policy Implementation",
-    y = "Number of Crashes",
-    caption = "Source: TIMS"
-  )
+  
+  geom_smooth(data = subset(rdit_data, Post == 1),
+              method = "lm",
+              se = TRUE) +
+  
+  geom_vline(xintercept = -0.5,
+             linetype = "dashed") +
+  
+  theme_minimal(base_size = 13) +
+  scale_x_continuous(breaks = seq(-24, 23, by = 1)) +
+  
+  labs(title = "Regression Discontinuity in Time: AB 413",
+       x = "Months Relative to January 2024",
+       y = "Monthly Pedestrian Crashes") 
+  
+# Save plot
+ggsave(filename = "initial-analysis/figs/rdit.png",
+       height = 10,
+       width = 20)
 
-model_data |>
-  group_by(Post) |>
-  summarise(avg_crashes = mean(Total_crashes)) |>
-  arrange(avg_crashes)
 
-
-# Method 2 ----------------------------------------------------------------
-
-# Clean and data for the model
-rdit_data <- tims_crashes |> 
-  filter(ACCIDENT_YEAR >= 2022) |> 
-  filter(PED_ACTION == "B" & INTERSECTION == "Y") |> 
-  mutate(
-    MONTH_DATE = floor_date(ymd(COLLISION_DATE), "month"),
-    Calendar_Month = format(MONTH_DATE, "%m"),
-    Time = interval(as.Date("2024-01-01"), MONTH_DATE) %/% months(1),
-    Post = ifelse(Time >= 0, 1, 0)
-  ) 
-
-# Dataset for Modeling: Group by covariates, Time and Calendar Month 
-model_data <- rdit_data |> 
-  group_by(Time, Post, Calendar_Month) |> 
-  summarise(Total_crashes = n(), 
+# Placebo Test ------------------------------------------------------------
+# Create the data for the control model
+placebo_data <- tims_updated |> 
+  filter(ACCIDENT_YEAR %in% c(2021, 2022, 2023)) |> 
+  filter(PED_ACTION == "B" & INTERSECTION == "Y") |>
+  filter_out(is.na(WEATHER_1)) |> 
+  filter_out(is.na(LIGHTING)) |> 
+  mutate(MONTH = floor_date(ymd(COLLISION_DATE), "month"),
+         Time = interval(as.Date("2022-07-01"), MONTH) %/% months(1),
+         Post = ifelse(Time >= 0, 1, 0),
+         Month_factor = factor((Time %% 12 + 12) %% 12)) |> 
+  group_by(Time, Post, Month_factor) |> 
+  summarise(Total_crashes = n(),
             .groups = "drop")
 
 
-## Adjusting for Monthly Seasonal Patterns ##
+# Run the model
+placebo_model <- lm(Total_crashes ~ Time + Post + Time*Post + 
+                   Month_factor,
+                 data = placebo_data)
+
+summary(placebo_model)
+
+# Plot the test
+ggplot(placebo_data,
+       aes(Time, Total_crashes)) +
+  geom_point(size = 2) +
   
-# Set March as the baseline month and create calendar dummy variable
-model_data$Calendar_Month <- relevel(factor(model_data$Calendar_Month),
-                                     ref = "03")  
-
-# Drop the intercept column with the baseline month
-cov_matrix <- model.matrix(~ Calendar_Month, data = model_data)[, -1]
-
-# Run the RDiT model using rdrobust
-robust_rdit <- rdrobust(
-  y = model_data$Total_crashes,
-  x = model_data$Time,
-  c = 0,                        # Cutoff is zero
-                        
-  covs = cov_matrix,            # Seasonal month controls
-  h = 24,                       # Bandwidth covering full 48-month windows
-  kernel = "uniform"            # Uniform weights for equal monthly evaluation
-)
-
-summary(robust_rdit)
-
-# Run the model using lm
-rdit_lm <- lm(Total_crashes ~ Time + Post + Time:Post + cov_matrix, 
-              data = model_data)
-
-summary(rdit_lm)
-
-## Seasonally-adjusted Plot ##
-
-# Linear model on the seasonal month dummies and time
-season_lm <- lm(Total_crashes ~ cov_matrix + time, 
-                data = model_data)
-summary(season_lm)
-
-# Time only model
-time_lm <- lm(Total_crashes ~ Time, 
-              data = model_data)
-summary(time_lm)
-
-# Extract model rediduals
-model_data$Crashes_Adjusted <- residuals(season_lm) + mean(model_data$Total_crashes)
-
-adjusted_plot_data <- model_data |> 
-  mutate(Total_crashes_adj = Crashes_Adjusted)
-
-ggplot(adjusted_plot_data, aes(x = Time, y = Total_crashes_adj)) +
-  # Plot the clean, de-seasonalized monthly data points
-  geom_point(size = 3.5, color = "blue") + 
+  geom_smooth(data = subset(placebo_data, Post == 0),
+              method = "lm",
+              formula = y ~ x + I(x^2),
+              se = TRUE) +
   
-  # Trend line before the law
-  geom_smooth(data = filter(adjusted_plot_data, Post == 0), 
-              method = "lm", 
-              formula = y ~ x, 
-              se = TRUE, 
-              color = "red") + 
+  geom_smooth(data = subset(placebo_data, Post == 1),
+              method = "lm",
+              formula = y ~ x + I(x^2),
+              se = TRUE) +
   
-  # Trend line after the law
-  geom_smooth(data = filter(adjusted_plot_data, Post == 1), 
-              method = "lm", 
-              formula = y ~ x, 
-              se = TRUE, 
-              color = "red") + 
+  geom_vline(xintercept = -0.5,
+             linetype = "dashed") +
   
-  # Clear vertical boundary line between Dec 2023 (-1) and Jan 2024 (0)
-  geom_vline(xintercept = -0.5, 
-             linetype = "dashed", 
-             linewidth = 1) + 
+  theme_minimal(base_size = 13) +
+  scale_x_continuous(breaks = seq(-18, 17, by = 1)) +
   
-  scale_x_continuous(breaks = seq(-24, 23, 1)) + 
-  theme_minimal(base_size = 13) + 
-  labs(
-    title = "RDiT Model: Pedestrian Crashes",
-    x = "Months Relative to Policy Implementation (Jan 2024 = 0)",
-    y = "Number of Crashes (Seasonally Adjusted)",
-    caption = "Source: Transportation Injury Mapping System (TIMS)"
-  )
+  labs(title = "RDiT Placebo Test: AB 413",
+       x = "Months Relative to July 2022",
+       y = "Monthly Pedestrian Crashes") 
+
+# Save plot
+ggsave(filename = "initial-analysis/figs/placebo.png",
+       height = 10,
+       width = 20)
 
 
-  
+
+
+
+
+
+
+
+
+
